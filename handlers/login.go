16fc73a9778a8d668102a7ec38c0d4e8109c7bd6
vishwa3596 <vishwa3556@gmail.com>
2022-05-13 13:31:48 +0530

    Added middleware

--- handlers/login.go
+++ handlers/login.go
@@ -1,6 +1,11 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 // field to keep in the login form
 /**
@@ -17,5 +22,10 @@ import "github.com/gin-gonic/gin"
 **/
 
 func Login(ctx *gin.Context) {
-
+	data, err := ioutil.ReadAll(ctx.Request.Body)
+	if err != nil {
+		http.Error(ctx.Writer, " Error Reading Request Data ", http.StatusBadRequest)
+		return
+	}
+	ctx.JSON(http.StatusAccepted, data)
 }
